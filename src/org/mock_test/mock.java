d82533f6c5c514eedee5d52cacb76e57c904dabe
santhanapriyap <priyasanthanap@gmail.com>
2022-07-21 10:46:16 +0530

    added

---
+++ src/org/mock_test/mock.java
@@ -0,0 +1,18 @@
+package org.mock_test;
+
+public class mock 
+{
+public static void main(String[] args) 
+{
+	int temp=0;
+int a[]= {11,2,3,4,5};
+for(int i=0; i<a.length;i++)
+{
+	temp=temp+a[i];
+	
+}
+System.out.println(temp);
+}
+}
+
+
